a1b25cb98408be3d5b1bf48eb2662984230cfdbf
Haf <haf@protonmail.ch>
2021-11-02 14:49:46 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js" ],
+  },
+
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+    hot: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ],
+};
