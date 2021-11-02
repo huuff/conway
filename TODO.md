43d7be708df63a7e55f585e32f8e75c0d0057828
Haf <haf@protonmail.ch>
2021-11-02 18:04:39 +0100

    controlling the size of the grid

---
+++ TODO.md
@@ -0,0 +1,1 @@
+* Default height so not every update to the row number pushes the controls down
--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,18 @@ const gameContainer = document.getElementById("main") as HTMLDivElement;
 const grid = new Grid(15, 15, 0.20);
 const display = new AsciiDisplay(gameContainer);
 
-const game = new Game(grid, display);
+let game = new Game(grid, display);
 game.start();
+
+document.getElementById("controls")!.addEventListener("change", () => {
+  const colNumber = getInputNumber("colNumber");
+  const rowNumber = getInputNumber("rowNumber");
+  let newGrid = new Grid(rowNumber, colNumber, 0.20);
+  game.stop();
+  game = new Game(newGrid, display);
+  game.start();
+})
+
+function getInputNumber(inputId: string): number {
+  return +(document.getElementById(inputId)! as HTMLInputElement).value 
+}
