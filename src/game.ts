823c5825ae9ff3445c9700d58656a4dfe56f8ec5
Haf <haf@protonmail.ch>
2021-11-03 15:36:23 +0100

    generating a display on each iteration

--- src/game.ts
+++ src/game.ts
@@ -5,30 +5,33 @@ import { Config } from "./config";
 
 export class Game {
   private readonly intervalId: number;
+  private currentConfig: Config;
   public grid: Grid;
-  public display: Display;
-  public speed: number;
 
   constructor(config: Config) {
-    this.updateConfig(config);
+    this.updateConfig(config); 
     this.intervalId = this.start();
   }
 
   public start(): number {
-    this.display.render(this.grid);
+    this.display().render(this.grid);
+
     return window.setInterval(() => {
       this.grid = update(this.grid);
-      this.display.render(this.grid);
-    }, 5000 / this.speed); 
+      this.display().render(this.grid);
+    }, 5000 / this.currentConfig.speed); 
   }
 
   public updateConfig(config: Config): void {
+    this.currentConfig = config;
     this.grid = Grid.fromConfig(config);
-    this.display = displayFromConfig(config);
-    this.speed = config.speed;
   }
 
   public stop(): void {
     window.clearInterval(this.intervalId);
+  }
+
+  private display(): Display {
+    return displayFromConfig(this.currentConfig);
   }
 }
